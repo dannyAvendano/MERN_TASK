74840c52cc79ff693e74e884f9abf3d39742b0ca
g-proebu09 <dannyfuv@gmail.com>
2020-12-10 11:31:26 -0500

    ajustes para deplyment

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({extended: true}))
 
 //Se crea el puerto de la app
-const PORT = process.env.PORT || 5000;
+const port = process.env.port || 5000;
 
 //importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -25,7 +25,7 @@ app.use('/api/tareas', require('./routes/tareas'));
 
 //Se arranca la app
 app.listen(
-    PORT, () => {
-        console.log('El servidor esta funcionando en el puerto...: ' + PORT)
+    port, '0.0.0.0', () => {
+        console.log('El servidor esta funcionando en el puerto...: ' + port)
     }
 )
